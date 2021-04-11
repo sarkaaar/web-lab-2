114321590c73f0fa38a09950d50d6463a8734ded
Abdul Wali <waliabdul144@gmail.com>
2021-04-11 17:30:30 +0500

    form

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 $(function () {
     loadRecipies();
+    $("#recipies").on("click", ".btn-danger", delrecipe);
 });
+
+
+function delrecipe() {
+
+    var btn = $(this);
+    var parent = btn.closest(".recipe");
+    let id = parent.attr("data-id");
+    console.log(id);
+    $.ajax({
+        
+        url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+        method: "DELETE",
+        success: function() { 
+            loadRecipies();
+        } 
+    })
+
+
+
+
+
+
+
+}
+
 
 function loadRecipies() {
     $.ajax({
@@ -13,11 +39,12 @@ function loadRecipies() {
             recipies.empty();
             for (var i = 0; i < response.length; i++) {
                 var rec = response[i]
-                recipies.append(`<div class="recipe"><h3>${rec.title}</h3>
+                recipies.append(`<div class="recipe" data-id="${rec._id}">
+                <h3>${rec.title}</h3>
+                <p>${rec.body} </p>
                 <button class="btn btn-danger">DELETE</button>
                 <button class="btn btn-warning">UPDATE</button>
-                <p>${rec.body}</p>
-                                    </div>`)
+            </div>`)
                 //    recipies.append("<div><h3>" + rec.title + "</h3></div>")
             }
         }
